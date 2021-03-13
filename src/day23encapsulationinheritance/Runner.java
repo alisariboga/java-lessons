195fcd8b946e3122c6d87d930db9fa6f47650bff
alsrbga <alican.sariboga.acs@gmail.com>
2021-03-13 17:48:58 +0000

    I added my study classes

--- src/day23encapsulationinheritance/Runner.java
+++ src/day23encapsulationinheritance/Runner.java
@@ -3,6 +3,11 @@ package day23encapsulationinheritance;
 public class Runner {
 
     public static void main(String[] args) {
-        
+        Dog dog1 = new Dog();
+        dog1.eat();
+        dog1.drink();
+        dog1.bark();
+        dog1.giveBirth();
+
     }
 }
