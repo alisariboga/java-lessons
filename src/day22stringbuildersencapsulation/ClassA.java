2b91df1684f3db1698a9aa9a0cae4f7c584fdfb3
alsrbga <alican.sariboga.acs@gmail.com>
2021-03-24 21:48:41 +0000

    some fixed

--- src/day22stringbuildersencapsulation/ClassA.java
+++ src/day22stringbuildersencapsulation/ClassA.java
@@ -1,5 +1,15 @@
 package day22stringbuildersencapsulation;
 
+/*
+Encapsulation Task
+1) Create class A
+2) Create class B
+3) Create a String, an int, and a boolean variable inside the class A and encapsulate them all
+4) Make String variable just readable
+5) Make int variable just updatable
+6) Make Boolean variable both readable and updatable
+ */
+
 public class ClassA {
     private String name = "Ali";
     private int age = 16;
